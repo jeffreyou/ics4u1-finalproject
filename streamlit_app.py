221ce7e467544acc7f5b141bf1dec726e939227e
Jeffrey Ou <jeffreyou19@gmail.com>
2023-12-02 08:51:37 -0500

    Installed Pytube and Streamlit on Path Directory, testing Pytube functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,13 @@
-print("test")
+import streamlit as st
+import pytube
+from pytube import Channel
+
+st.title("League VOD Manager")
+
+champion = st.text_input("Enter a champion:")
+if champion:
+    st.write(f"Searching for {champion}...")
+
+c = Channel('https://www.youtube.com/c/ChallengerReplays/videos')
+for url in c.video_urls:
+    st.write(url, c.video_urls)
